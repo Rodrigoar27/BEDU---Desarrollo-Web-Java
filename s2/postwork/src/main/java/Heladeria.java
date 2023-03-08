1709d1ebc319770ec3e4d7e4b2b0779385b12b09
Rodrigoar27 <90352792+Rodrigoar27@users.noreply.github.com>
2023-03-08 14:30:53 -0600

    Update Heladeria.java

--- s2/postwork/src/main/java/Heladeria.java
+++ s2/postwork/src/main/java/Heladeria.java
@@ -1,18 +1,17 @@
 
 public class Heladeria {
 
-
+//En esta clase simularemos un ticket de compra.
     public static void main(String [] args) {
         Helado helado = new HeladoBase();
 
-
         helado = new Cobertura(helado);
-        helado = new Granola(helado);
-        helado = new Mermelada(helado);
-        helado = new Galleta(helado);
-        helado = new Topping(helado);
+        helado = new CoberturaGranola(helado);
+        helado = new CoberturaMermelada(helado);
+        helado = new CoberturaGalleta(helado);
+        helado = new CoberturaTopping(helado);
 
-
+//Enviamos a consola el ticket de compra del usuario, empleando los dos métodos definidos al inicio. 
         System.out.println("Gracias por comprar en nuestra heladeria");
         System.out.println("A continuacion se desglosa su pedido y el precio total:");
         System.out.println(helado.getIngredientes());
